b6c890ae591e5d35612a238c3a97e8723a756b7c
Duk <dhugkar@gmail.com>
2021-10-31 00:23:21 +0530

    Added Action no_play,snake and ladder

--- src/com/bridgelabz/game/Game.java
+++ src/com/bridgelabz/game/Game.java
@@ -4,8 +4,28 @@ public class Game {
 
 	public static void main(String[] args) {
 
+		final int NO_PLAY = 0;
+		final int snake = 1;
+		final int ladder = 2;
+		int player = 0;
 		int dice = (int) ((Math.random() * 10) % 6) + 1;
-		System.out.println("Dice is rolled " + dice);
+
+		int act = (int) ((Math.random() * 10) % 3);
+
+		switch (act) {
+		
+		case NO_PLAY:
+			break;
+		case snake:
+			player = player - dice;
+			break;
+		case ladder:
+			player = player + dice;
+			break;
+		
+		}
+
+		System.out.println("player position " + player);
 
 	}
 }
