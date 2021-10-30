3380f33e86ef75bd19946a0b0326f4ca7aca4845
Duk <dhugkar@gmail.com>
2021-10-31 01:13:54 +0530

    Added Report step by step

--- src/com/bridgelabz/game/Game.java
+++ src/com/bridgelabz/game/Game.java
@@ -9,6 +9,7 @@ public class Game {
 		final int snake = 1;
 		final int ladder = 2;
 		int player = 0;
+		int NO_OF_DICE_ROLLED = 0;
 
 		while (player < MAX_POSITION) {
 
@@ -16,30 +17,35 @@ public class Game {
 
 			int act = (int) ((Math.random() * 10) % 3);
 
+			String action = "";
 			switch (act) {
 
 			case NO_PLAY:
+				action = "no_play";
 				break;
-			
+
 			case snake:
+				action = "snake ";
 				int temp1 = player - dice;
 				if (temp1 < 0)
 					player = 0;
 				else
 					player = temp1;
 				break;
-			
-			case ladder:
 
+			case ladder:
+				action = "ladder";
 				int temp2 = player + dice;
 				if (temp2 <= 100)
 					player = temp2;
-				
+
 				break;
 
 			}
+			NO_OF_DICE_ROLLED++;
 
-			System.out.println("player position " + player);
+			System.out.println("Die:" + dice + " \t action: " + action + " \t player position " + player);
 		}
+		System.out.println("Total number of dice rolled to win the game is " + NO_OF_DICE_ROLLED);
 	}
 }
